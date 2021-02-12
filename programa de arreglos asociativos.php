485c5184a68daf4c8c4ae5c00bb201d9f33c34c0
patote605 <52435316+patote605@users.noreply.github.com>
2021-02-11 19:48:21 -0500

    Add files via upload

---
+++ programa de arreglos asociativos.php
@@ -0,0 +1,71 @@
+<?php
+
+$empleado1 = array( 'nombre'=>"Nombre: jorge", 'apellido'=>"Apellido: Urdaneta", 'cedula'=>"C.I:28545896", 'trabajo'=>"Trabaja en FPG", 'departamento'=>"Encargado de la Direccion General", 'sueldo'=>"gana 300$ mensual");
+
+echo $empleado1 ['nombre'];
+echo "<br>";
+echo "<br>";
+echo $empleado1 ['apellido'];
+echo "<br>";
+echo "<br>";
+echo $empleado1 ['cedula'];
+echo "<br>";
+echo "<br>";
+echo $empleado1 ['trabajo'];
+echo "<br>";
+echo "<br>";
+echo $empleado1 ['departamento'];
+echo "<br>";
+echo "<br>";
+echo $empleado1 ['sueldo'];
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$empleado2 = array( 'nombre'=>"Nombre: Paul", 'apellido'=>"Apellido: Bodington", 'cedula'=>"C.I:10414863", 'trabajo'=>"Trabaja en FPG", 'departamento'=>"Encargado de la Direccion Operativa", 'sueldo'=>"gana 200$ mensual");
+
+echo $empleado2 ['nombre'];
+echo "<br>";
+echo "<br>";
+echo $empleado2 ['apellido'];
+echo "<br>";
+echo "<br>";
+echo $empleado2 ['cedula'];
+echo "<br>";
+echo "<br>";
+echo $empleado2 ['trabajo'];
+echo "<br>";
+echo "<br>";
+echo $empleado2 ['departamento'];
+echo "<br>";
+echo "<br>";
+echo $empleado2 ['sueldo'];
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$empleado3 = array( 'nombre'=>"Nombre: Maria", 'apellido'=>"Apellido: Bodington", 'cedula'=>"C.I:7760797", 'trabajo'=>"Trabaja en FPG", 'departamento'=>"Encargado de la Direccion Creativa", 'sueldo'=>"gana 150$ mensual");
+
+echo $empleado3 ['nombre'];
+echo "<br>";
+echo "<br>";
+echo $empleado3 ['apellido'];
+echo "<br>";
+echo "<br>";
+echo $empleado3 ['cedula'];
+echo "<br>";
+echo "<br>";
+echo $empleado3 ['trabajo'];
+echo "<br>";
+echo "<br>";
+echo $empleado3 ['departamento'];
+echo "<br>";
+echo "<br>";
+echo $empleado3 ['sueldo'];
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+?>
